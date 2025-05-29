d984d6a9e4fcdfdc51464e9121b44a913d1db902
Sanghyun Yun <545austin@naver.com>
2025-05-29 23:27:15 +0900

    javascript&python_capital_transition_revision

--- python/basic/04_capital_transition.py
+++ python/basic/04_capital_transition.py
@@ -1,2 +1,12 @@
 str = input()
 print(str.swapcase())
+
+# upper()
+a = "Python is easy to learn"
+print(a.upper()) # PYTHON IS EASY TO LEARN
+print(a.lower()) # python is easy to learn
+
+a = "PythonMaster"
+a.swapcase()
+print(a) # PythonMaster --> 원본 출력
+print(a.swapcase()) #pYTHONmASTER" --> swapcase()는 대문자를 소문자로, 소문자를 대문자로 바꿔줌
